fd7497fd69b7a8c216ef219b27d409c03f39e0ed
arvindj1986 <arvindj1986@gmail.com>
2020-02-22 04:03:54 -0500

    first release

---
+++ abc.c
@@ -0,0 +1,9 @@
+void main()
+{
+printf("Hello");
+int a=2;
+int b=3;
+printf("Addition of two numbers is %d", &(a+b));
+
+
+}
---
+++ sec.java
@@ -0,0 +1,43 @@
+ class sec {
+
+    public static void main(String[] args) {
+
+       
+        System.out.print("Enter two numbers: ");
+
+        // nextDouble() reads the next double from the keyboard
+        double first = reader.nextDouble();
+        double second = reader.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /): ");
+
+
+        double result;
+
+        switch(operator)
+        {
+            case '+':
+                result = first + second;
+                break;
+
+            case '-':
+                result = first - second;
+                break;
+
+            case '*':
+                result = first * second;
+                break;
+
+            case '/':
+                result = first / second;
+                break;
+
+            // operator doesn't match any case constant (+, -, *, /)
+            default:
+                System.out.printf("Error! operator is not correct");
+                return;
+        }
+
+   
+    
+
